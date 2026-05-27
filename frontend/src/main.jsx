3505ffa284c2834dd65028a895e5e4b5cd1262b3
DrionGegollaj <dg73875@ubt-uni.net>
2026-05-27 17:43:08 +0200

    docs: cross-browser test checklist

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,9 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import App from './App.jsx';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+      <ToastContainer position="top-right" autoClose={3000} theme="colored" />
+    </BrowserRouter>
+  </React.StrictMode>
 );
---
+++ frontend/src/utils/toast.js
@@ -0,0 +1,10 @@
+import { toast } from 'react-toastify';
+
+export const notify = {
+  success: (msg) => toast.success(msg),
+  error: (msg) => toast.error(msg),
+  info: (msg) => toast.info(msg),
+  warn: (msg) => toast.warn(msg),
+};
+
+export default notify;
